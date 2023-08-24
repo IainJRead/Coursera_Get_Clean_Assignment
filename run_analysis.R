868ee14b2816d72d18821dac259148a232eec08a
IainJRead <iainjamesread@gmail.com>
2023-08-24 10:59:49 +0100

    Added sys.nframe() == 0 check
    When the script is run directly, the run_analysis() function is called.
    When used as source, the function will not be executed.
    Allows the script to be a library or run directly.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+#!/usr/bin/Rscript
+
 ## Getting and Cleaning Data Week 4 Assignment
 ## 1. Import test and train datasets from UCI HAR Dataset
 ## 2. Merge into a single dataset
@@ -8,46 +10,52 @@
 ##    for each activity and subject
 
 library(dplyr)
+library(plyr)
 
-# Read in data sets from UCI HAR directory and merge
-X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-X_full <- rbind(X_train, X_test)
+run_analysis <- function(){
+    # Read in data sets from UCI HAR directory and merge
+    X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+    X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+    X_full <- rbind(X_train, X_test)
+    
+    # Use features.txt to get X column names
+    features <- read.table("UCI HAR Dataset/features.txt")
+    colnames(features) <- c("Column.Index", "Feature.Label")
+    
+    # Keep only the mean and stdev per measurement
+    names_to_keep <- features[grepl("mean\\(\\)", features$Feature.Label) | grepl("std\\(\\)", features$Feature.Label),]
+    
+    # Select from merged dataset based on names, and relabel columns accordingly
+    X_full <- select(X_full, names_to_keep$Column.Index)
+    colnames(X_full) <- names_to_keep$Feature.Label
+    
+    subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+    subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+    subject_full <- rbind(subject_train, subject_test)
+    colnames(subject_full) <- c("Subject")
+    
+    Y_train <- read.table("UCI HAR Dataset/train/Y_train.txt")
+    Y_test <- read.table("UCI HAR Dataset/test/Y_test.txt")
+    Y_full <- rbind(Y_train, Y_test)
+    
+    colnames(Y_full) <- c("Activity_Category")
+    
+    # Use activity_labels.txt to get activity names
+    activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+    colnames(activities) <- c("Category", "Activity")
+    
+    # Assign labels to each activity in Y, then drop the numerical category
+    Y_full <- merge(Y_full, activities, by.x="Activity_Category", by.y="Category", all.x=TRUE)
+    Y_full <- select(Y_full, -Activity_Category)
+    
+    # Combine all three datasets together
+    XY_full <- cbind(subject_full, Y_full, X_full)
+    
+    # Generate dataset of mean values per variable, grouped by Subject and Activity
+    summary_set <- ddply(XY_full, .(Subject, Activity), numcolwise(mean))   
+}
 
-# Use features.txt to get X column names
-features <- read.table("UCI HAR Dataset/features.txt")
-colnames(features) <- c("Column.Index", "Feature.Label")
-
-# Keep only the mean and stdev per measurement
-names_to_keep <- features[grepl("mean\\(\\)", features$Feature.Label) | grepl("std\\(\\)", features$Feature.Label),]
-
-# Select from merged dataset based on names, and relabel columns accordingly
-X_full<- select(X_full, names_to_keep$Column.Index)
-colnames(X_full) <- names_to_keep$Feature.Label
-
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
-subject_full <- rbind(subject_train, subject_test)
-colnames(subject_full) <- c("Subject")
-
-Y_train <- read.table("UCI HAR Dataset/train/Y_train.txt")
-Y_test <- read.table("UCI HAR Dataset/test/Y_test.txt")
-Y_full <- rbind(Y_train, Y_test)
-
-colnames(Y_full) <- c("Activity_Category")
-
-# Use activity_labels.txt to get activity names
-activities <- read.table("UCI HAR Dataset/activity_labels.txt")
-colnames(activities) <- c("Category", "Activity")
-
-# Assign labels to each activity in Y, then drop the numerical category
-Y_full <- merge(Y_full, activities, by.x="Activity_Category", by.y="Category", all.x=TRUE)
-Y_full <- select(Y_full, -Activity_Category)
-
-# Combine all three datasets together
-XY_full <- cbind(subject_full, Y_full, X_full)
-
-# Generate dataset of mean values per variable, grouped by Subject and Activity
-summary_set <- ddply(XY_full, .(Subject, Activity), numcolwise(mean))
-
-
+# runs only when script is run by itself
+if (sys.nframe() == 0){
+    run_analysis()
+}
